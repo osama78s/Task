6d820c6f7e35191faa41b1e38e240554dbcbe035
osama <osamasaif242@gmail.com>
2024-08-20 08:34:50 +0300

    Task

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Context from './Components/Context/Context.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <Router>
+  <Router basename='/Task/'>
     <Context>
       <App />
     </Context>
